f393cb3fe59ebca9e2063d44873ca9400c6fb9ed
wyn <weiyanan@quantibio.com>
2019-11-21 15:38:48 +0800

    添加包

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/snow/test/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/snow/test/AndroidStudioProjects/flutter_app_test"
-export "FLUTTER_TARGET=/Users/snow/test/AndroidStudioProjects/flutter_app_test/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/snow/test/AndroidStudioProjects/Flutter-Dart"
+export "FLUTTER_TARGET=/Users/snow/test/AndroidStudioProjects/Flutter-Dart/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/snow/test/flutter/bin/cache/artifacts/engine/ios"
