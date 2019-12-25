085d0f862b645948c8ba4c964b81db44a2afc1c8
wyn <weiyanan@quantibio.com>
2019-12-25 15:27:30 +0800

    路由

--- lib/router/index.dart
+++ lib/router/index.dart
@@ -1,18 +1,26 @@
-import 'package:flutter_app_test/pages/cate.dart';
-import 'package:flutter_app_test/pages/index.dart';
-import 'package:flutter_app_test/pages/movie_list.dart';
-import 'package:flutter_app_test/pages/share.dart';
-
+import 'package:flutter/material.dart';
+import 'package:flutter_dart/pages/index.dart';
+import 'package:flutter_dart/pages/login.dart';
+import 'package:flutter_dart/pages/cate.dart';
+import 'package:flutter_dart/pages/movie_list.dart';
+import 'package:flutter_dart/pages/share.dart';
+import 'package:flutter_dart/pages/movie_detail.dart';
+import 'package:flutter_dart/pages/todo_detail.dart';
 
 const pageIndex = '/index';
+const pageLogin = '/login';
 const pageCate = '/cate';
 const pageMovieList = '/movie';
 const pageShare = '/share';
-
+const pageMovieDetail = '/movie-detail';
+const pageTodoDetail = '/todo/:id';
 
 var RoutePath = {
   '$pageIndex': (context) => MyApp(),
+  '$pageLogin': (context) => LoginPage(),
   '$pageCate': (context) => Cate(),
   '$pageMovieList': (context) => MoviePage(),
-  '$pageShare': (context) => SharePage()
+  '$pageShare': (context) => SharePage(),
+  '$pageMovieDetail': (context) => MoiveDetailPage(detailId: ModalRoute.of(context).settings.arguments),
+  '$pageTodoDetail': (context) => TodoDetailPage(todo: ModalRoute.of(context).settings.arguments),
 };
